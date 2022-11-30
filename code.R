85c68cf23a034122235697da57686f1b37994021
justarandomnameduh <97799964+justarandomnameduh@users.noreply.github.com>
2022-11-30 12:00:17 +0700

    Add files via upload

--- code.R
+++ code.R
@@ -1,38 +1,61 @@
-# Load data from csv file
-raw_data <- read.csv("house_price.csv")
-data <- raw_data[,c('price', 'sqft_living', 'floors',
-                    'condition','sqft_above', 'sqft_living15')]
+library(ggcorrplot)
+library(corrr)
+library(data.table)
+library(lmtest)
+library(MASS)
+library(vip)
+library(car)
+library(tidyr)
+library(dplyr)
+library(corrplot)
+library(RColorBrewer)
+library(summarytools)
+library(tidyverse)
+library(caret)
 
-# Get correlation between each feature
-corr_df <- as.data.frame(cor(data, use='complete.obs'))
-col_list <- list()
-for(col in colnames(corr_df)){
-  col_list <- append(col_list,paste("corr_", col))
-}
-colnames(corr_df) <- col_list
+### Data preprocessing
+housePriceData <- read.csv("house_price.csv")
+housePrice <- housePriceData[,c('price', 'sqft_living', 'floors',
+                                'condition','sqft_above', 'sqft_living15')]
 
-# Calculate mean and variance of each feature
-mv_df <- data.frame(feature=character(),
-                    mean=double(),
-                    variance=double())
-for (col in colnames(data)){
-  mv_df[nrow(mv_df) + 1,] <- c(col, mean(data[,col], na.rm=TRUE),
-                               var(data[,col], na.rm=TRUE))
-}
-rownames(mv_df) <- mv_df$feature
-mv_df <- mv_df[,c('mean', 'variance')]
+options(max.print = 50)
+housePrice
 
-# Merge 2 df above into 1
-stat_df <- merge(corr_df, mv_df, by=0, all.x=TRUE)
-rownames(stat_df) <- stat_df$Row.names
-write.csv(stat_df, "stat_analyze.csv", row.names=TRUE)
-## Graphing
+# Check for NaN values
+sum(is.na(housePrice))
+
+# There are NaN values in the data
+# As the number of observations is large enough,
+# We decide to remove datapoints with NaN values
+housePrice <- housePrice %>% drop_na()
+
+# Check for duplication
+duplicated(housePrice) %>% sum()
+
+# There are duplication so we remove it from the dataframe
+housePrice <- distinct(housePrice)
+
+### Descriptive statistic:
+CR <- cor(housePrice)
+View(CR)
+png("corr_plot.png", width = 650, height = 650)
+
+corrplot(CR, type = "full", order="hclust",
+         col = brewer.pal(n=3,name="RdYlBu"),
+         main = "\nCorrelation Heatmap")
+dev.off()
+
+descr(as.data.frame(housePrice), transpose = TRUE,
+      stats = c('mean', 'sd', 'min', 'max', 'med',
+                'Q1', 'IQR', 'Q3'))
+
+### Visualization
 # Histogram
 png("hist_plot.png", width = 1300, height = 1000)
 par(mfrow=c(2,3))
 
-for (col in colnames(data)) {
-  hist(data[,col], labels=data[,col], xlab = col, main=col, breaks=35,
+for (col in colnames(housePrice)) {
+  hist(housePrice[,col], labels=housePrice[,col], xlab = col, main=col, breaks=35,
        cex.lab=2, cex.axis=2, cex.main=3, cex.sub=2)
 }
 dev.off()
@@ -41,8 +64,8 @@ dev.off()
 png("box_plot.png", width = 1300, height = 1000)
 par(mfrow=c(2,3))
 
-for (col in colnames(data)) {
-  boxplot(data[,col], labels=data[,col], xlab = col, main=col, breaks=35, 
+for (col in colnames(housePrice)) {
+  boxplot(housePrice[,col], labels=housePrice[,col], xlab = col, main=col, breaks=35, 
           cex.lab=2, cex.axis=2, cex.main=3, cex.sub=2)
 }
 dev.off()
@@ -50,35 +73,24 @@ dev.off()
 # Pairs plot
 png("pairs_plot.png", width = 4000, height = 4000)
 
-pairs(data, cex.labels=7, pch=19, cex = 4, cex.axis = 3)
+pairs(housePrice, cex.labels=7, pch=19, cex = 4, cex.axis = 5)
 dev.off()
 
-
-## Linear Regression
-# Using price as a target feature, we will remove all data points where price value is NaN
-data <- na.omit(data)
-
-## Uncomment these 2 lines to install required packages for 
-## train-test split function and linear regression model
-#install.packages("tidyverse")
-#install.packages("caret")
-
-library(tidyverse)
-library(caret)
-
+### Linear Regression
+# Split data into train and test set
 set.seed(1)
 
-random_sample <- createDataPartition(data$price, p=0.8, list=FALSE)
-train_data <- data[random_sample,]
-test_data <- data[-random_sample,]
-# Building a linear regression model
+random_sample <- createDataPartition(housePrice$price, p=0.8, list=FALSE)
+train_data <- housePrice[random_sample,]
+test_data <- housePrice[-random_sample,]
 
-
+# Create a standard linear regression model
 linreg <- lm(price ~., data = train_data)
 predictions <- predict(linreg, test_data)
 error_df <- data.frame( R2 = R2(predictions, test_data$price),
-            RMSE = RMSE(predictions, test_data$price),
-            MAE = MAE(predictions, test_data$price))
+                        RMSE = RMSE(predictions, test_data$price),
+                        MAE = MAE(predictions, test_data$price))
 coef_list <- data.frame(linreg$coefficients)
+summary(linreg)
 write.csv(coef_list, "linreg_coef.csv", row.names=TRUE)
 write.csv(error_df, "linreg_error.csv", row.names=TRUE)
